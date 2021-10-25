ba174434bac9b0710b29cdfc3c01d4fb301860d2
Ryan Lee <45303876+ryan11lee@users.noreply.github.com>
2021-10-24 23:32:32 -0700

    Change split function to account for url changes

--- webScraper.py
+++ webScraper.py
@@ -15,14 +15,14 @@ def get_text(url):
         print(tag.get_text())
         justText.append(tag.get_text().replace("\t", "").replace("\r", "").replace("\n", ""))
 
-    with open("%s" %url.split("/")[4] + ".txt", "w+" ) as file:
+    with open("%s" %url.split("/").pop() + ".txt", "w+" ) as file:
         for item in justText:
             file.write(item)
 
 
 url = "https://www.theverge.com/22684421/apple-iphone-13-mini-review"
 url2 = "https://www.theverge.com/21522988/iphone-12-review"
-
+url3 = "https://www.theverge.com/2019/9/17/20869456/apple-iphone-11-review-camera-price-budget-battery-screen-size-features"
 
 try:
     os.mkdir("text")
@@ -31,5 +31,5 @@ except FileExistsError:
     
 os.chdir("text")
 
-for u in [url,url2]:
+for u in [url,url2,url3]:
     get_text(u)
